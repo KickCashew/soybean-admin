d18b15d9a8bf091ae9c87f66a75819f3f9c959ae
KickCashew <h1687204657@163.com>
2024-06-02 23:02:06 +0800

    feat: Add keyboard shortcut for global search modal

--- src/layouts/modules/global-search/index.vue
+++ src/layouts/modules/global-search/index.vue
@@ -1,11 +1,27 @@
 <script lang="ts" setup>
 import { useBoolean } from '@sa/hooks';
+import { onKeyStroke } from '@vueuse/core';
 import { $t } from '@/locales';
+import { useAppStore } from '@/store/modules/app';
 import SearchModal from './components/search-modal.vue';
 
 defineOptions({ name: 'GlobalSearch' });
 
 const { bool: show, toggle } = useBoolean();
+
+const appStore = useAppStore();
+
+onKeyStroke(
+  ['Control', 'k'],
+  e => {
+    e.preventDefault();
+    if (e.ctrlKey && e.key === 'k' && !show.value) {
+      appStore.closeThemeDrawer();
+      toggle();
+    }
+  },
+  { dedupe: true }
+);
 </script>
 
 <template>
